8eb88e2eb78dd8f6895b8d3377d4c7ea71dea4fd
Henry Hazan <henry@work.capital>
2018-04-22 16:16:43 +0300

    first commit

---
+++ calc/lib/calc.ex
@@ -0,0 +1,18 @@
+defmodule Calc do
+  @moduledoc """
+  Documentation for Calc.
+  """
+
+  @doc """
+  Add two elemets, i.e. terms, one over another.
+
+  ## Examples
+
+      iex> Calc.add(1, 1)
+      3
+
+  """
+  def add(term1, term2) do
+    term1 + term2
+  end
+end
